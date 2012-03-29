d9efe1ce5d8061f129e00e98c76bf6322e50cec5
Joachim Nelson <pimpimmi@gmail.com>
2012-03-29 22:50:45 +0200

    das

--- src/view/ProductionPane.java
+++ src/view/ProductionPane.java
@@ -3,8 +3,7 @@ package view;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.FocusEvent;
-import java.awt.event.FocusListener;
+import java.util.ArrayList;
 import java.util.Set;
 
 import javax.swing.DefaultListModel;
@@ -21,35 +20,34 @@ import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 import javax.swing.table.DefaultTableModel;
 
+import db.Product;
 import db.ProductMap;
 
 
 public class ProductionPane extends BasicPane{
 	
+	DefaultTableModel ingrTableModel;
 	ProductMap pr;
 	DefaultListModel listModel;
-	JTextField quantity;
-	JTable table;
+	JList list;
+	JTextField quantityText;
 	int id = 1;
 
 	public ProductionPane(ProductMap pr) {
-		super();
 		this.pr = pr;
+		setUpPane();
 	}
 	
 	
 	public JComponent createLeftPanel() {
 		listModel = new DefaultListModel();
-		JList list = new JList(listModel);
+		list = new JList(listModel);
 		list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		list.setPrototypeCellValue("123456789012");
 		list.setFixedCellWidth(200);
-// TODO	Set<String> productNames = pr.keySet();
-//		for(String s : productNames)
-//			listModel.addElement(s);
-		listModel.addElement("hej");
-		listModel.addElement("hej2");
-		
+		Set<String> productNames = pr.keySet();
+		for(String s : productNames)
+			listModel.addElement(s);
 		list.addListSelectionListener(new ListFocusListener());
 		return new JScrollPane(list);
 		
@@ -66,18 +64,20 @@ public class ProductionPane extends BasicPane{
 	public JComponent createTopPanel() {		
 		JLabel p1 = new JLabel("Quantity: ");
 
-		quantity = new JTextField(5);
-		
+		quantityText = new JTextField(5);
+		quantityText.setText("1");
 		JPanel p = new JPanel();
 		p.add(p1);
-		p.add(quantity);
+		p.add(quantityText);
 		
 		return p;
 	}
 	
 	public JComponent createMiddlePanel() {
-		table = new JTable();
-		table.setModel(new DefaultTableModel( null, new String [] {"Ingredient","Required", "Available"} ));
+		JTable table = new JTable();
+		ingrTableModel = new DefaultTableModel( null, new String [] {"Ingredient","Required", "Available"});
+		table.setModel(ingrTableModel);
+		table.setEnabled(false);
 		JScrollPane simulationScrollPane = new JScrollPane();
 		for(int i = 0; i < table.getColumnCount(); i++)
 			table.getColumnModel().getColumn(i).setResizable(false);
@@ -98,7 +98,20 @@ public class ProductionPane extends BasicPane{
 
 		@Override
 		public void valueChanged(ListSelectionEvent e) {
-			
+			Product p = pr.getProduct((String)list.getSelectedValue());
+			for(int i = 0; i < ingrTableModel.getRowCount(); i++)
+				ingrTableModel.removeRow(0);
+			ArrayList<String> ingredients = p.getIngredients();
+			ArrayList<Integer> neededAmount = p.getQuantities();
+			int quantity = Integer.valueOf(quantityText.getText());
+			for(int i = 0; i < ingredients.size(); i++){
+				String[] row = new String[3];
+				row[0] = ingredients.get(i);
+				row[1] = String.valueOf(neededAmount.get(i)*quantity);
+				row[2] = "";
+				ingrTableModel.addRow(row);
+			}
+				
 		}		
 	}
 	
